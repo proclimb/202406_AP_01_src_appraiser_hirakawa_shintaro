29ae5cfbee0356ee5120ff8501fa9f7fa76d3312
Shintaro Hirakawa <2024ap01_hirakawa@x-ide.sakura.ne.jp>
2024-08-01 14:06:41 +0900

    演算子を「!==」から「!=」に変更

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -43,7 +43,7 @@ function subAdminUser()
 						<td class="list_td<?php print $i; ?>"><?php print $password; ?></td>
 						<td class="list_td<?php print $i; ?>"><?php print fnAuthorityName($authority); ?></td>
 						<td class="list_td<?php print $i; ?>">
-							<?php if ($userNo !== '1') : ?>
+							<?php if ($userNo != 1) : ?>
 								<a href="javascript:fnAdminUserDeleteCheck(<?php print $userNo; ?>,'<?php print $name; ?>');">削除</a>
 							<?php endif ?>
 						</td>
